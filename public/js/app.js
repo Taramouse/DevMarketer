ba97c32b23b2b6ac31bab85c907240a487308fdb
taramouse <support@optimized4u.org>
2017-11-01 16:42:50 +0000

    Compiled

--- public/js/app.js
+++ public/js/app.js
@@ -30123,7 +30123,7 @@ var Component = normalizeComponent(
   __vue_scopeId__,
   __vue_module_identifier__
 )
-Component.options.__file = "resources/assets/js/components/slugWidget.vue"
+Component.options.__file = "resources\\assets\\js\\components\\slugWidget.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key.substr(0, 2) !== "__"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] slugWidget.vue: functional components are not supported with templates, they should use render functions.")}
 
